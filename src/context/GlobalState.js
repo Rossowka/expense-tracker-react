78c3752af8aba8d4b0475d4d786fe5653d5c2af0
Sara Rossow <rossowka@icloud.com>
2022-10-28 15:59:29 +0200

    feature: global state

---
+++ src/context/GlobalState.js
@@ -0,0 +1,45 @@
+import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
+
+// Initial state
+const initialState = {
+  transactions: [
+    { id: 0, payee: 'Salary', amount: 1800, category: 'Income' },
+    { id: 1, payee: 'Rent', amount: -600, category: 'Immediate Obligations' },
+    { id: 2, payee: 'Utilities', amount: -200, category: 'Immediate Obligations' },
+    { id: 3, payee: 'Groceries', amount: -150, category: 'True Expenses' },
+    { id: 4, payee: 'Gift Card', amount: 25, category: 'Unknown' },
+    { id: 5, payee: 'Gym Membership', amount: -30, category: 'Quality of Life' },
+]
+}
+
+// Create context
+export const GlobalContext = createContext(initialState);
+
+// Provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  // Actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id
+    });
+  }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction
+    });
+  }
+
+  return (<GlobalContext.Provider value={{
+    transactions: state.transactions,
+    deleteTransaction,
+    addTransaction
+  }}>
+    {children}
+  </GlobalContext.Provider>);
+}
