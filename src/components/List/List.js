72acfd5fff92c2f42265d76df1652a8219b004ee
Sara Rossow <rossowka@icloud.com>
2022-10-31 14:38:02 +0100

    add: transaction component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,11 +1,8 @@
 import React, { useContext } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
 import { GlobalContext } from '../../context/GlobalState';
-import Icon from './Icon';
+import Transaction from './Transaction';
 
 import './styles.scss';
-import styles from './../../_base.module.scss';
 
 function List() {
     const { transactions } = useContext(GlobalContext);
@@ -15,22 +12,7 @@ function List() {
             <h3>Transactions</h3>
             <dl>
                 {transactions.map(transaction => (
-                    <div className='element'>
-                        <Icon category={transaction.category} />
-                        <div className='text'>
-                            <dt>{transaction.payee}</dt>
-                            <dd>{transaction.category}</dd>
-                        </div>
-                        <div
-                            className='money'
-                            style={{color: transaction.amount < 0 ? styles.negativeColor : styles.positiveColor}}
-                        >
-                            <dt>€{transaction.amount}</dt>
-                        </div>
-                        <button>
-                            <FontAwesomeIcon icon={faTrashCan} size="sm"/>
-                        </button>
-                    </div>
+                    <Transaction key={transaction.id} transaction={transaction}/>
                 ))}
             </dl>
         </div>
---
+++ src/components/List/Transaction.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
+import Icon from './Icon';
+
+import './styles.scss';
+import styles from './../../_base.module.scss';
+
+function Transaction({ transaction }) {
+    return (
+        <div className='element'>
+            <Icon category={transaction.category} />
+            <div className='text'>
+                <dt>{transaction.payee}</dt>
+                <dd>{transaction.category}</dd>
+            </div>
+            <div
+                className='money'
+                style={{color: transaction.amount < 0 ? styles.negativeColor : styles.positiveColor}}>
+                <dt>€{transaction.amount}</dt>
+            </div>
+            <button>
+                <FontAwesomeIcon icon={faTrashCan} size="sm"/>
+            </button>
+        </div>
+    )
+}
+
+export default Transaction;
