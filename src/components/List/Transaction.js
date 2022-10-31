9d098d248269232d3f9df66f0a9a50b61506518f
Sara Rossow <rossowka@icloud.com>
2022-10-31 15:46:05 +0100

    feature: delete transaction

--- src/components/List/Transaction.js
+++ src/components/List/Transaction.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
 import Icon from './Icon';
+import { GlobalContext } from '../../context/GlobalState';
 
 import './styles.scss';
 import styles from './../../_base.module.scss';
 
 function Transaction({ transaction }) {
+    const { deleteTransaction } = useContext(GlobalContext);
     const sign = transaction.amount < 0 ? '-' : '+';
 
     return (
@@ -21,7 +23,7 @@ function Transaction({ transaction }) {
                 style={{color: transaction.amount < 0 ? styles.negativeColor : styles.positiveColor}}>
                 <dt>{sign}€{Math.abs(transaction.amount)}</dt>
             </div>
-            <button>
+            <button onClick={() => deleteTransaction(transaction.id)}>
                 <FontAwesomeIcon icon={faTrashCan} size="sm"/>
             </button>
         </div>
